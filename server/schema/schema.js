9160ee85c2783fa00df82dd3029f5980c889994f
lyndonpett <lyndonpett98@gmail.com>
2022-08-12 11:27:55 -0500

    Adding task 2

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,23 @@
 const graphql = require('graphql');
 const { GraphQLString, GraphQLInt } = graphql;
 
+const tasks = [
+  {
+    id: '1',
+    title: 'Create your first webpage',
+    weight: 1,
+    description:
+      'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)',
+  },
+  {
+    id: '2',
+    title: 'Structure your webpage',
+    weight: 1,
+    description:
+      'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order',
+  },
+];
+
 const TaskType = new graphql.GraphQLObjectType({
   name: 'Task',
   fields: () => ({
@@ -19,7 +36,9 @@ const RootQuery = new graphql.GraphQLObjectType({
       args: {
         id: { type: GraphQLString },
       },
-      resolve(parentValue, args) {},
+      resolve: (parent, args) => {
+        return tasks.find((task) => task.id === args.id);
+      },
     },
   }),
 });
