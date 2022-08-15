0cd96261be8fc35be4c2eb7bce5a47b4c4346254
lyndonpett <lyndonpett98@gmail.com>
2022-08-15 12:01:54 -0500

    First try for tasks 9-13

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -2,14 +2,17 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 app.use(
   '/graphql',
   graphqlHTTP({
     schema,
-    graphiql: true,
+    graphiql: true
   })
 );
 app.listen(4000, () => {
